b1e2850ac135dee4d3f91af13ee1fc5427ff997b
Trip Volpe <trip.volpe@gmail.com>
2015-11-22 20:42:06 -0800

    Make some noise with SDL and snes_spc!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,63 @@
+extern crate sdl2;
+extern crate snes_spc;
+
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::audio::{AudioCallback, AudioSpecDesired};
+use snes_spc::SnesSpc;
+
+struct SpcPlayer {
+    emulator: SnesSpc
+}
+
+impl AudioCallback for SpcPlayer {
+    type Channel = i16;
+    fn callback(&mut self, out: &mut [i16]) {
+        self.emulator.play(out).unwrap();
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let sdl_context = sdl2::init().unwrap();
+    let video = sdl_context.video().unwrap();
+
+    let window = video.window("Tarnished", 800, 500)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let mut renderer = window.renderer()
+        .accelerated()
+        .build()
+        .unwrap();
+
+    // Start making noise
+    let audio_subsystem = sdl_context.audio().unwrap();
+    let desired_spec = AudioSpecDesired {
+        freq: Some(32000),
+        channels: Some(2),
+        samples: None
+    };
+
+    let audio = audio_subsystem.open_playback(None, desired_spec, |spec| {
+        println!("Audio initialized: {:?}", spec);
+        SpcPlayer {
+            emulator: SnesSpc::from_file("assets/otherworldly.spc").unwrap()
+        }
+    }).unwrap();
+
+    audio.resume();
+
+    'mainloop: loop {
+        for event in sdl_context.event_pump().unwrap().poll_iter() {
+            match event {
+                Event::Quit{..} |
+                Event::KeyDown {keycode: Some(Keycode::Escape), ..} => {
+                    break 'mainloop
+                },
+                _ => { }
+            }
+        }
+        renderer.present();
+    }
 }
