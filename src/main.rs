2d5c35ddc1c0fdab26900d367db25b170f484572
Trip Volpe <trip.volpe@gmail.com>
2016-01-13 02:01:32 -0800

    Add Textbox, a composite visual element (currently a bit broken)

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 extern crate sdl2;
 extern crate snes_spc;
 
+use std::path::Path;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::audio::{AudioCallback, AudioSpecDesired};
+use sdl2::rect::Rect;
 use snes_spc::SnesSpc;
 
 mod scene;
@@ -17,6 +19,55 @@ impl AudioCallback for SpcPlayer {
     type Channel = i16;
     fn callback(&mut self, out: &mut [i16]) {
         self.emulator.play(out).unwrap();
+    }
+}
+
+struct Textbox {
+    base: String,
+    bounds: Rect,
+}
+
+impl Textbox {
+    fn new(base: &str, bounds: Rect) -> Textbox {
+        Textbox {
+            base: base.to_string(),
+            bounds: bounds,
+        }
+    }
+
+    fn part(&self, name: &str) -> String {
+        Path::new(&self.base).join(name).to_string_lossy().into_owned()
+    }
+
+    fn render(&self) -> Vec<Sprite> {
+        // XXX: allow querying these dimensions from asset system, or
+        // provide a way to draw with configurable alignment
+        let cw = 8;
+        let ch = 8;
+        let x = self.bounds.x();
+        let y = self.bounds.y();
+        let w = self.bounds.width() as i32;
+        let h = self.bounds.height() as i32;
+
+        // XXX: sprite instructions don't have stretched drawing; perhaps
+        // the right way to do this is with more primitives, e.g., Sprite,
+        // Stretched, Flipped, etc. Asset-relative instructions might be
+        // neat too, in a hypothetical metalanguage:
+        //      base(self.base,
+        //          sprite("tl", (x-cw, y-ch)),
+        //          stretched(sprite("t", (x, y-ch)), (w, 8)),
+        //          hflipped(sprite("tl", (x+w, y-ch))))
+        //          
+        vec![
+            Sprite::new(&self.part("tl"), (x-cw, y-ch)),
+            Sprite::new(&self.part("t"),  (x,    y-ch)),
+            Sprite::new(&self.part("tr"), (x+w,  y-ch)),
+            Sprite::new(&self.part("l"),  (x-cw, y)),
+            Sprite::new(&self.part("r"),  (x+w,  y)),
+            Sprite::new(&self.part("bl"), (x-cw, y+h)),
+            Sprite::new(&self.part("b"),  (x,    y+h)),
+            Sprite::new(&self.part("br"), (x+w,  y+h)),
+        ]
     }
 }
 
@@ -57,6 +108,8 @@ fn main() {
     let (mut x, mut y) = (0, 0);
     let starman = Sprite::new("assets/starmanjr_lg", (280, 100));
     let starman2 = Sprite::new("assets/starmanjr_lg", (300, 100));
+    let textbox = Textbox::new("assets/box",
+                               Rect::new_unwrap(16, 16, 64, 48));
 
     'mainloop: loop {
         for event in sdl_context.event_pump().unwrap().poll_iter() {
@@ -81,10 +134,22 @@ fn main() {
             }
         }
 
+        // XXX: note that box must be rendered before creating scene, since
+        // scene borrows references to all the instructions added to it. This
+        // is perhaps an API weakness; might end up just boxing visibles.
+        let rendered_box = textbox.render();
+
         let mut scene = Scene::new();
         scene.set_viewport((x, y));
         scene.add(&starman, 1);
         scene.add(&starman2, 0);
+
+        // XXX: doesn't make much sense to specify separate z-index for
+        // every piece of this textbox when rendering piecewise to scene
+        for p in &rendered_box {
+            scene.add(p, 2);
+        }
+
         scene.present(&mut renderer);
     }
 }
