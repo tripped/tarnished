8462c96a27067892a329f77083df6fc9fe765100
Trip Volpe <trip.volpe@gmail.com>
2016-01-18 22:34:21 -0800

    Jot down a note about Textbox::render and the z-index/type discomfort

--- src/main.rs
+++ src/main.rs
@@ -40,6 +40,18 @@ impl Textbox {
         Path::new(&self.base).join(name).to_string_lossy().into_owned()
     }
 
+    // XXX: Note that here we've given Textbox a method which just dumps a
+    // whole bunch of sprites into a vector. Later, something needs to take
+    // those sprites and put them into a Scene. Unfortunately, Scene::add()
+    // is where information about z-index comes in, and Textbox probably has
+    // an opinion about that. At the very least, conventional wisdom dictates
+    // that all the bits of the textbox should have the same z-level.
+    //
+    // Given that our design philosophy emphasizes decoupling and value-based
+    // programming, the lazy approach of giving render() a &Scene parameter
+    // is obviously a no-go. Other options include promoting the Instruction
+    // type to public and making it the output of typical render functions,
+    // and the input to Scene::add().
     fn render(&self) -> Vec<Sprite> {
         let w = self.bounds.width() as usize;
         let h = self.bounds.height() as usize;
@@ -56,7 +68,7 @@ impl Textbox {
             sprite(&self.part("r"),  HPos::Left(r),       VPos::Stretch(y, h)),
             sprite(&self.part("bl"), HPos::Right(x),      VPos::Top(b)),
             sprite(&self.part("b"),  HPos::Stretch(x, w), VPos::Top(b)),
-            sprite(&self.part("br"), HPos::Left(r),      VPos::Top(b)),
+            sprite(&self.part("br"), HPos::Left(r),       VPos::Top(b)),
         ]
     }
 }
