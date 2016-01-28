c53965e7b9274b8a8fa43e136a2b0ff08c2ac02c
Trip Volpe <trip.volpe@gmail.com>
2016-01-27 21:17:07 -0800

    Use RefCell for renderer and texture cache to provide interior mutability

--- src/scene.rs
+++ src/scene.rs
@@ -1,6 +1,7 @@
 extern crate sdl2;
 extern crate sdl2_image;
 
+use std::cell::RefCell;
 use std::cmp::Ordering;
 use std::collections::BinaryHeap;
 use std::collections::HashMap;
@@ -30,8 +31,8 @@ pub enum VPos {
 
 /// Renderer: 1. n. A person or thing that renders.
 pub struct Renderer {
-    renderer: sdl2::render::Renderer<'static>,
-    textures: HashMap<String, Texture>,
+    renderer: RefCell<sdl2::render::Renderer<'static>>,
+    textures: RefCell<HashMap<String, Texture>>,
     offset: (i32, i32),
 }
 
@@ -48,8 +49,8 @@ fn load_texture(asset: &str, renderer: &sdl2::render::Renderer) -> Texture {
 impl Renderer {
     pub fn new(renderer: sdl2::render::Renderer<'static>) -> Renderer {
         Renderer {
-            renderer: renderer,
-            textures: HashMap::new(),
+            renderer: RefCell::new(renderer),
+            textures: RefCell::new(HashMap::new()),
             offset: (0, 0),
         }
     }
@@ -58,12 +59,12 @@ impl Renderer {
         self.offset = offset;
     }
 
-    pub fn clear(&mut self) {
-        self.renderer.clear();
+    pub fn clear(&self) {
+        self.renderer.borrow_mut().clear();
     }
 
-    pub fn present(&mut self) {
-        self.renderer.present();
+    pub fn present(&self) {
+        self.renderer.borrow_mut().present();
     }
 
     /// Load a texture if it does not already exist in cache
@@ -74,27 +75,25 @@ impl Renderer {
     /// to draw the texture we just requested. Currently the only way to fix
     /// this would be to redesign this system so that renderer and the texture
     /// cache aren't in the same struct.
-    fn ensure_texture(&mut self, asset: &str) {
-        if !self.textures.contains_key(asset) {
-            self.textures.insert(asset.into(),
-                load_texture(asset, &self.renderer));
+    fn ensure_texture(&self, asset: &str) {
+        if !self.textures.borrow().contains_key(asset) {
+            self.textures.borrow_mut().insert(asset.into(),
+                load_texture(asset, &self.renderer.borrow_mut()));
         }
     }
 
     /// Query information about a texture by its asset name.
-    ///
-    /// XXX: having all these functions be &mut self is a pain, but it's
-    /// required because of the interface of sdl2::render. Possibly look into
-    /// using Cell to achieve internal mutability and clean up this interface?
-    pub fn query(&mut self, asset: &str) -> TextureQuery {
+    pub fn query(&self, asset: &str) -> TextureQuery {
         self.ensure_texture(asset);
-        let tex = self.textures.get(&asset.to_string()).unwrap();
+        let cache = self.textures.borrow();
+        let tex = cache.get(&asset.to_string()).unwrap();
         tex.query()
     }
 
-    pub fn draw(&mut self, asset: &str, hpos: HPos, vpos: VPos) {
+    pub fn draw(&self, asset: &str, hpos: HPos, vpos: VPos) {
         self.ensure_texture(asset);
-        let tex = self.textures.get(&asset.to_string()).unwrap();
+        let cache = self.textures.borrow();
+        let tex = cache.get(&asset.to_string()).unwrap();
 
         let query = tex.query();
         let width = query.width as i32;
@@ -118,21 +117,22 @@ impl Renderer {
         let dst = Rect::new_unwrap(x1, y1, (x2 - x1) as u32, (y2 - y1) as u32);
         let dst = dst.offset(offx, offy).unwrap();
 
-        self.renderer.copy(&tex, None, Some(dst));
+        self.renderer.borrow_mut().copy(&tex, None, Some(dst));
     }
 
     /// Draw the nth tile of specified size from a given image asset, at a
     /// specific position, assuming top-left alignment.
     /// XXX: Assumes that tiles are laid out in a single horizontal strip.
-    pub fn draw_tile(&mut self, tileset: &str, n: u32, w: u32, h: u32,
+    pub fn draw_tile(&self, tileset: &str, n: u32, w: u32, h: u32,
                      x: i32, y: i32) {
         self.ensure_texture(tileset);
-        let tex = self.textures.get(&tileset.to_string()).unwrap();
+        let cache = self.textures.borrow();
+        let tex = cache.get(&tileset.to_string()).unwrap();
 
         let src = Rect::new_unwrap((n * w) as i32, 0, w, h);
         let (offx, offy) = self.offset;
         let dst = Rect::new_unwrap(x, y, w, h).offset(offx, offy).unwrap();
-        self.renderer.copy(&tex, Some(src), Some(dst));
+        self.renderer.borrow_mut().copy(&tex, Some(src), Some(dst));
     }
 }
 
