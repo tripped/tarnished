89467bb9cba303916ea705600ebd564ac15dc311
Trip Volpe <trip.volpe@gmail.com>
2016-02-28 18:29:30 -0800

    Render a full row of tiles in TilePicker

--- src/tilepicker.rs
+++ src/tilepicker.rs
@@ -1,4 +1,4 @@
-use scene::{Rectangle, Tile};
+use scene::{Rectangle, Tex};
 use sdl2::rect::Rect;
 use sdl2::pixels::Color;
 
@@ -23,15 +23,32 @@ impl TilePicker {
 
     /// Render the tileset picker.
     /// XXX: this is kind of an experiment in different render structures
-    pub fn render(&self) -> (Vec<Rectangle>, Vec<Tile>) {
+    pub fn render(&self) -> (Vec<Rectangle>, Vec<Tex>) {
         // First, fill the whole space with a sexy dark rectangle
         let rects = vec![
             Rectangle::filled(self.rect, Color::RGBA(32, 32, 32, 255))];
 
-        // Draw the first tile!
-        let tiles = vec![
-            Tile::new(&self.tileset, 0, self.tile_width, self.tile_height,
-                      0, 0)];
+        // Draw some tiles!
+        // XXX: this duplicates the code in Renderer::draw_tile that determines
+        // tileset layout, but that didn't seem like the appropriate place to
+        // have it anyway. Dedup and move when appropriate.
+
+        let mut tiles = Vec::new();
+        let (w, h) = (64, 64); // target render dimensions for each tile
+        let padding = 1;
+
+        let n = self.rect.width() / w;
+
+        for i in (0..n) {
+            let src_x = i * self.tile_width;
+            let src = Rect::new_unwrap(
+                (i * self.tile_width) as i32, 0,
+                self.tile_width, self.tile_height);
+            let dst = Rect::new_unwrap(
+                (i * (w + padding)) as i32, padding as i32, w, h);
+
+            tiles.push(Tex::new(&self.tileset, Some(src), dst));
+        }
 
         (rects, tiles)
     }
