5dbad99cf3b3d2831deb3f95dde5268d8ac22c55
Trip Volpe <trip.volpe@gmail.com>
2016-04-05 23:51:51 -0700

    Add momentum to static physics entity

--- src/physics.rs
+++ src/physics.rs
@@ -1,6 +1,15 @@
 use carboxyl::Signal;
 
+#[derive(Clone, Copy)]
+pub struct Momentum {
+    direction: f32,
+    magnitude: f32,
+}
+
 /// An object with nonvarying position
-pub fn static_position(x: f32, y: f32) -> Signal<(f32, f32)> {
-    Signal::new((x, y))
+pub fn static_position(x: f32, y: f32)
+        -> (Signal<(f32, f32)>, Signal<Momentum>) {
+    (Signal::new((x, y)), Signal::new(Momentum {
+        direction: 0.0, magnitude: 0.0,
+    }))
 }
