fcaf5a56ed060e8409b167e671d63d36941992f0
Trip Volpe <trip.volpe@gmail.com>
2016-04-06 20:48:43 -0700

    Update Momentum and Position definitions

--- src/physics.rs
+++ src/physics.rs
@@ -1,15 +1,38 @@
 use carboxyl::Signal;
 
 #[derive(Clone, Copy)]
-pub struct Momentum {
-    direction: f32,
-    magnitude: f32,
+pub struct Vector2D {
+    x: f32,
+    y: f32,
+}
+
+impl Vector2D {
+    pub fn new(x: f32, y: f32) -> Vector2D {
+        Vector2D { x: x, y: y }
+    }
+}
+
+#[derive(Clone, Copy)]
+pub struct Momentum(Vector2D);
+
+impl Momentum {
+    pub fn new(x: f32, y: f32) -> Momentum {
+        Momentum(Vector2D { x: x, y: y })
+    }
+}
+
+#[derive(Clone, Copy)]
+pub struct Position(Vector2D);
+
+impl Position {
+    pub fn new(x: f32, y: f32) -> Position {
+        Position(Vector2D { x: x, y: y })
+    }
 }
 
 /// An object with nonvarying position
 pub fn static_position(x: f32, y: f32)
-        -> (Signal<(f32, f32)>, Signal<Momentum>) {
-    (Signal::new((x, y)), Signal::new(Momentum {
-        direction: 0.0, magnitude: 0.0,
-    }))
+        -> (Signal<Position>, Signal<Momentum>) {
+    (Signal::new(Position::new(x, y)),
+     Signal::new(Momentum::new(0.0, 0.0)))
 }
