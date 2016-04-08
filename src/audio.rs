0f22e6d9910c1c26b2f4e71d9dc15b96ee0e464f
Trip Volpe <trip.volpe@gmail.com>
2016-04-07 21:55:48 -0700

    audio: add a Mixer prototype

--- src/audio.rs
+++ src/audio.rs
@@ -19,3 +19,32 @@ impl AudioCallback for SpcPlayer {
         self.emulator.play(out).unwrap();
     }
 }
+
+/// Manages a set of channels
+pub struct Mixer {
+    channels: Vec<Box<AudioCallback<Channel = i16>>>,
+}
+
+impl Mixer {
+    pub fn new() -> Mixer {
+        Mixer {
+            channels: Vec::new(),
+        }
+    }
+
+    pub fn play<S: AudioCallback<Channel = i16> + 'static>(
+            &mut self, source: S) {
+        self.channels.push(Box::new(source));
+    }
+}
+
+impl AudioCallback for Mixer {
+    type Channel = i16;
+
+    // XXX: assumes one channel
+    fn callback(&mut self, out: &mut [i16]) {
+        if self.channels.len() > 0 {
+            self.channels[0].callback(out);
+        }
+    }
+}
