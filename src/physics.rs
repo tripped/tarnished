6565915fe836cca132304231a839b8e62564508b
Trip Volpe <trip.volpe@gmail.com>
2016-04-09 21:09:36 -0700

    Prefix unused physics functions with _ to suppress unused function warnings

--- src/physics.rs
+++ src/physics.rs
@@ -7,7 +7,7 @@ pub struct Vector2D {
 }
 
 impl Vector2D {
-    pub fn new(x: f32, y: f32) -> Vector2D {
+    pub fn _new(x: f32, y: f32) -> Vector2D {
         Vector2D { x: x, y: y }
     }
 }
@@ -16,7 +16,7 @@ impl Vector2D {
 pub struct Momentum(Vector2D);
 
 impl Momentum {
-    pub fn new(x: f32, y: f32) -> Momentum {
+    pub fn _new(x: f32, y: f32) -> Momentum {
         Momentum(Vector2D { x: x, y: y })
     }
 }
@@ -25,14 +25,14 @@ impl Momentum {
 pub struct Position(Vector2D);
 
 impl Position {
-    pub fn new(x: f32, y: f32) -> Position {
+    pub fn _new(x: f32, y: f32) -> Position {
         Position(Vector2D { x: x, y: y })
     }
 }
 
 /// An object with nonvarying position
-pub fn static_position(x: f32, y: f32)
+pub fn _static_position(x: f32, y: f32)
         -> (Signal<Position>, Signal<Momentum>) {
-    (Signal::new(Position::new(x, y)),
-     Signal::new(Momentum::new(0.0, 0.0)))
+    (Signal::new(Position::_new(x, y)),
+     Signal::new(Momentum::_new(0.0, 0.0)))
 }
