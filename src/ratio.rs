0d25622f736baa158ac0c52cd430f173094f6cd2
Trip Volpe <trip.volpe@gmail.com>
2016-04-22 20:49:10 -0700

    Add Scalable impls for u8*u32, u16*u32, i8*u32, i16*u32

--- src/ratio.rs
+++ src/ratio.rs
@@ -35,6 +35,11 @@ impl Scalable<i32, i32> for i32 {}
 // Scaling an i32 by Ratio<u32> involves first converting to i64.
 impl Scalable<u32, i64> for i32 {}
 
+impl Scalable<u32, u32> for u8 {}
+impl Scalable<u32, u32> for u16 {}
+impl Scalable<u32, i64> for i8 {}
+impl Scalable<u32, i64> for i16 {}
+
 // Note that there is no implementation of Scalable<i32, X> for u32; scaling
 // an unsigned integer by a signed ratio makes no sense since the desired
 // result type (the same as the source integer's type) can never be negative.
@@ -55,4 +60,28 @@ fn scale_i32() {
 fn scale_i32_by_u32_ratio() {
     let r = Ratio::new(1u32, 2u32);
     assert_eq!(-4.scale(r), -2);
+}
+
+#[test]
+fn scale_u8_by_u32_ratio() {
+    let r = Ratio::new(1523u32, 1546u32);
+    assert_eq!(232u8.scale(r), 228);
+}
+
+#[test]
+fn scale_u16_by_u32_ratio() {
+    let r = Ratio::new(7251u32, 492124u32);
+    assert_eq!(51221u16.scale(r), 754);
+}
+
+#[test]
+fn scale_i8_by_u32_ratio() {
+    let r = Ratio::new(5u32, 25u32);
+    assert_eq!(-100.scale(r), -20);
+}
+
+#[test]
+fn scale_i16_by_u32_ratio() {
+    let r = Ratio::new(5u32, 20u32);
+    assert_eq!(-12.scale(r), -3);
 }
